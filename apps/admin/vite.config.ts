c5e7229491f6ad41eeeee33c7dc0bd69ee700082
lgp2345 <lgp2345@126.com>
2025-12-12 23:43:25 +0800

    chore: enhance Vite configuration in admin app by adding TanStack Router plugin for improved routing capabilities

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,10 +1,22 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import path from "node:path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+      routesDirectory: "./src/routes",
+      generatedRouteTree: "./src/routeTree.gen.ts",
+      routeFileIgnorePrefix: "-",
+      quoteStyle: "single",
+    }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -17,5 +29,3 @@ export default defineConfig({
     sourcemap: true,
   },
 });
-
-
