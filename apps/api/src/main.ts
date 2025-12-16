2e0ad6c057e204b65f621823464b06950de1a1d6
lgp2345 <lgp2345@126.com>
2025-12-16 23:35:27 +0800

    chore: add Fastify as a dependency and update main.ts to use FastifyAdapter for NestJS application

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,11 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter, type NestFastifyApplication } from "@nestjs/platform-fastify";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  await app.listen(3000, "0.0.0.0");
 }
 
 void bootstrap();
